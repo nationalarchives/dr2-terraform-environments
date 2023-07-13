74f8f5ba0dadf1fb65b3630f5fb3e83e5bdfbcdd
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-07-13 17:34:19 +0100

    Update lambda and topic names

--- entity_event_generator.tf
+++ entity_event_generator.tf
@@ -1,6 +1,6 @@
 locals {
-  entity_event_lambda_name = "${local.environment}-entity-event-lambda"
-  entity_event_topic_name  = "${local.environment}-entity-event-topic"
+  entity_event_lambda_name = "${local.environment}-entity-event-generator-lambda"
+  entity_event_topic_name  = "${local.environment}-entity-event-generator-topic"
   last_polled_table_name   = "${local.environment}-entity-event-lambda-updated-since-query-start-datetime"
   entity_event_topic_arn   = "arn:aws:sns:eu-west-2:${data.aws_caller_identity.current.account_id}:${local.entity_event_topic_name}"
 }
@@ -12,7 +12,7 @@ module "entity_event_cloudwatch_event" {
   lambda_event_target_arn = "arn:aws:lambda:eu-west-2:${data.aws_caller_identity.current.account_id}:function:${local.entity_event_lambda_name}"
 }
 
-module "entity_event_lambda" {
+module "entity_event_generator_lambda" {
   source        = "git::https://github.com/nationalarchives/da-terraform-modules//lambda"
   function_name = local.entity_event_lambda_name
   handler       = "uk.gov.nationalarchives.Lambda::handleRequest"
@@ -20,7 +20,7 @@ module "entity_event_lambda" {
     entity_event_policy = templatefile("${path.module}/templates/iam_policy/entity_event_lambda_policy.json.tpl", {
       account_id                 = data.aws_caller_identity.current.account_id
       lambda_name                = local.entity_event_lambda_name
-      dynamo_db_arn              = module.entity_event_last_updated_table.table_arn
+      dynamo_db_arn              = module.entity_event_lambda_updated_since_query_start_datetime_table.table_arn
       secrets_manager_secret_arn = aws_secretsmanager_secret.preservica_secret.arn
       sns_arn                    = local.entity_event_topic_arn
     })
@@ -38,17 +38,17 @@ module "entity_event_lambda" {
   }
 }
 
-module "entity_event_last_updated_table" {
+module "entity_event_lambda_updated_since_query_start_datetime_table" {
   source        = "git::https://github.com/nationalarchives/da-terraform-modules//dynamo"
   hash_key      = "id"
   hash_key_type = "S"
   table_name    = local.last_polled_table_name
 }
 
-module "entity_event_sns_out" {
+module "entity_event_generator_topic" {
   source = "git::https://github.com/nationalarchives/da-terraform-modules//sns"
   sns_policy = templatefile("${path.module}/templates/sns/entity_event_topic_policy.json.tpl", {
-    lambda_role_arn = module.entity_event_lambda.lambda_role_arn
+    lambda_role_arn = module.entity_event_generator_lambda.lambda_role_arn
     sns_topic       = local.entity_event_topic_arn
   })
   tags       = {}
