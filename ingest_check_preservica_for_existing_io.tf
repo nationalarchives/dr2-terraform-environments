8c23631eb6adaca3099295e6d96fc82589a7a56d
techncl <nigel.lutale@nationalarchives.gov.uk>
2024-04-19 16:35:07 +0100

    Add 'Existing IO-check' Lambda & update step function

---
+++ ingest_check_preservica_for_existing_io.tf
@@ -0,0 +1,34 @@
+locals {
+  ingest_check_preservica_for_existing_io_lambda_name = "${local.environment}-ingest-check-preservica-for-existing-io"
+}
+module "ingest_check_preservica_for_existing_io_lambda" {
+  source          = "git::https://github.com/nationalarchives/da-terraform-modules//lambda"
+  function_name   = local.ingest_check_preservica_for_existing_io_lambda_name
+  handler         = "uk.gov.nationalarchives.Lambda::handleRequest"
+  timeout_seconds = 60
+  policies = {
+    "${local.ingest_check_preservica_for_existing_io_lambda_name}-policy" = templatefile(
+      "./templates/iam_policy/ingest_check_preservica_for_existing_io_policy.json.tpl", {
+        account_id                 = var.account_number
+        lambda_name                = local.ingest_check_preservica_for_existing_io_lambda_name
+        dynamo_db_arn              = module.files_table.table_arn
+        secrets_manager_secret_arn = aws_secretsmanager_secret.preservica_secret.arn
+      }
+    )
+  }
+  memory_size = local.java_lambda_memory_size
+  runtime     = local.java_runtime
+  plaintext_env_vars = {
+    PRESERVICA_SECRET_NAME = aws_secretsmanager_secret.preservica_secret.name
+    PRESERVICA_API_URL     = data.aws_ssm_parameter.preservica_url.value
+    DYNAMO_TABLE_NAME      = local.files_dynamo_table_name
+  }
+  vpc_config = {
+    subnet_ids         = module.vpc.private_subnets
+    security_group_ids = [module.outbound_https_access_only.security_group_id]
+  }
+  tags = {
+    Name      = local.ingest_check_preservica_for_existing_io_lambda_name
+    CreatedBy = "dp-terraform-environments"
+  }
+}
