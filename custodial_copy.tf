f17c5d0d90bace9565d8d863bb404287a392fb4a
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2024-07-29 18:27:10 +0100

    Use the correct queue in the policy

--- custodial_copy.tf
+++ custodial_copy.tf
@@ -26,7 +26,7 @@ resource "aws_iam_group_policy" "custodial_copy_group_policy" {
   policy = templatefile("${path.module}/templates/iam_policy/custodial_copy_policy.json.tpl", {
     account_id                 = data.aws_caller_identity.current.account_id
     secrets_manager_secret_arn = aws_secretsmanager_secret.preservica_secret.arn
-    custodial_copy_queue       = module.dr2_custodial_copy_notifications_queue.sqs_arn
+    custodial_copy_queue       = module.dr2_custodial_copy_queue.sqs_arn
     database_builder_queue     = module.dr2_custodial_copy_db_builder_queue.sqs_arn
     management_account_id      = module.config.account_numbers["mgmt"]
   })
