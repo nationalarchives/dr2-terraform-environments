5fbcabd0fa8b889a52a3d5f40bcee3fe3eb51ab0
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2024-10-03 13:18:29 +0100

    Update policy to allow nacl rule changes

--- runbooks.tf
+++ runbooks.tf
@@ -1,19 +1,20 @@
-module "remove_security_groups_role" {
+module "remove_all_nacl_rules_role" {
   source = "git::https://github.com/nationalarchives/da-terraform-modules//iam_role"
   assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
     account_id = data.aws_caller_identity.current.account_id,
   repo_filters = jsonencode(["repo:nationalarchives/dr2-runbooks:environment:intg", "repo:nationalarchives/dr2-runbooks:environment:staging", "repo:nationalarchives/dr2-runbooks:environment:prod"]) })
-  name = "${local.environment}-dr2-runbook-remove-security-group-rule"
+  name = "${local.environment}-dr2-runbook-remove-all-nacl-rules"
   policy_attachments = {
-    remove_security_group_policy = module.remove_security_groups_policy.policy_arn
+    remove_all_nacl_rules_policy = module.remove_all_nacl_rules_policy.policy_arn
   }
   tags = {}
 }
 
-module "remove_security_groups_policy" {
+module "remove_all_nacl_rules_policy" {
   source = "git::https://github.com/nationalarchives/da-terraform-modules//iam_policy"
-  name   = "${local.environment}-dr2-runbooks-policy"
-  policy_string = templatefile("${path.module}/templates/iam_policy/remove_security_groups.json.tpl", {
-    security_group_arn = "arn:aws:ec2:eu-west-2:${data.aws_caller_identity.current.account_id}:security-group/${module.outbound_https_access_only.security_group_id}"
+  name   = "${local.environment}-dr2-runbooks-remove-all-nacl-rules-policy"
+  policy_string = templatefile("${path.module}/templates/iam_policy/remove_all_nacl_rules.json.tpl", {
+    public_nacl_arn  = module.vpc.public_nacl_arn
+    private_nacl_arn = module.vpc.private_nacl_arn
   })
 }
