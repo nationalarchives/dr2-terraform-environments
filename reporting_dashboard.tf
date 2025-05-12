82ccfd14e8728d87e8878c28bac110908b859b19
sparkhi <4743002+sparkhi@users.noreply.github.com>
2025-05-12 16:13:44 +0100

    formatting

--- reporting_dashboard.tf
+++ reporting_dashboard.tf
@@ -38,9 +38,9 @@ module "dr2_ingest_reporting_lambda" {
 }
 
 resource "aws_s3_object" "index_html" {
-    bucket = local.ingest_reporting_bucket_name
-    key    = "index.html"
-    source = "${path.module}/website/reporting_dashboard/index.html"
+  bucket = local.ingest_reporting_bucket_name
+  key    = "index.html"
+  source = "${path.module}/website/reporting_dashboard/index.html"
 }
 
 resource "aws_cloudfront_origin_access_control" "reporting_access_control" {
