d0f9579c68c6da49741ed3419c481f4e6ee0870a
sparkhi <4743002+sparkhi@users.noreply.github.com>
2025-09-05 12:25:50 +0100

    Made changes to attach trigger to the lambda

--- ingest_metric_collector.tf
+++ ingest_metric_collector.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 locals {
   ingest_metric_collector_lambda_name = "${local.environment}-dr2-ingest-metric-collector"
+  invocation_event_rule_name          = "${local.environment}-dr2-metric-collector-invocation-rule"
 }
 
 module "dr2_ingest_metric_collector_lambda" {
@@ -19,4 +28,24 @@ module "dr2_ingest_metric_collector_lambda" {
   tags = {
     Name = local.ingest_metric_collector_lambda_name
   }
+}
+
+resource "aws_cloudwatch_event_rule" "every_minute" {
+  name                = local.invocation_event_rule_name
+  description         = "triggers the lambda every minute"
+  schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_trigger" {
+  rule      = aws_cloudwatch_event_rule.every_minute.name
+  target_id = "dr2_ingest_metric_collector_lambda"
+  arn       = module.dr2_ingest_metric_collector_lambda.lambda_arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = local.ingest_metric_collector_lambda_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.every_minute.arn
 }
