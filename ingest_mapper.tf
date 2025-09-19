8efce253cfee50c3050f082106a59669f726c2b6
sparkhi <4743002+sparkhi@users.noreply.github.com>
2025-09-19 12:44:14 +0100

    new vpc for accessing discovery

--- ingest_mapper.tf
+++ ingest_mapper.tf
@@ -26,3 +26,27 @@ module "dr2_ingest_mapper_lambda" {
     Name = local.ingest_mapper_lambda_name
   }
 }
+
+resource "aws_vpc_endpoint" "discovery" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.vpce.eu-west-2.vpce-svc-030613f5fe9f42a77"
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [module.discovery_inbound_https.security_group_id]
+}
+
+module "discovery_inbound_https" {
+  source      = "git::https://github.com/nationalarchives/da-terraform-modules//security_group"
+  common_tags = {}
+  description = "A security group to allow inbound access to discovery VPC endpoint from lambda security group"
+  name        = "${local.environment}-dr2-discovery-inbound-https"
+  vpc_id      = module.vpc.vpc_id
+  ingress_security_group_rules = [{
+    port              = 443
+    description       = "Inbound access from lambda security group"
+    security_group_id = module.outbound_https_access_only.security_group_id
+    }
+  ]
+}
+
